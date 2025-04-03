456890a01f59ae7e5a68da2459978399cffaee12
chrishson <chris.h.son@gmail.com>
2025-04-02 20:55:50 -0700

    indentation

--- app/frontend/pages/TaskLists/Show.tsx
+++ app/frontend/pages/TaskLists/Show.tsx
@@ -2,14 +2,14 @@ import { Head } from '@inertiajs/react'
 
 export default function TaskListsShow({ task_list }: { task_list: any }) {
 
-	return (
-		<>
-			<Head title="Task List" />
-			<div className="max-w-2xl mx-auto p-4">
-				<h1 className="text-2xl font-bold mb-4">
-					{`${task_list.name} ${task_list.id}`}
-				</h1>
-			</div>
-		</>
-	)
+  return (
+    <>
+      <Head title="Task List" />
+      <div className="max-w-2xl mx-auto p-4">
+        <h1 className="text-2xl font-bold mb-4">
+          {`${task_list.name} ${task_list.id}`}
+        </h1>
+      </div>
+    </>
+  )
 }
